96c2088e06b98759ad335c5282a3eb663eaa9c1d
Georgi Velikov <gmvelikov@gmail.com>
2022-01-04 17:10:42 +0200

    Improving exception message

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/exception/AppExceptionHandler.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/exception/AppExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
@@ -14,14 +15,24 @@ public class AppExceptionHandler {
 
     @ExceptionHandler(value = RestApiException.class)
     public ResponseEntity<Object> handleRestApiException(RestApiException ex, WebRequest request) {
-	ExceptionMessageRest exception = new ExceptionMessageRest(new Date(), ex.getMessage(),
-		ex.getSourceExceptionMessage());
+	ExceptionMessageRest exception = new ExceptionMessageRest(new Date(), getPath(request), getMethod(request),
+		ex.getMessage(), ex.getSourceExceptionMessage());
 	return new ResponseEntity<>(exception, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<Object> handleException(Exception ex, WebRequest request) {
-	ExceptionMessageRest exception = new ExceptionMessageRest(new Date(), ex.getMessage());
+	ExceptionMessageRest exception = new ExceptionMessageRest(new Date(), getPath(request), getMethod(request),
+		ex.getMessage());
 	return new ResponseEntity<>(exception, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private String getPath(WebRequest request) {
+	// TODO must check when there is param in the path
+	return ((ServletWebRequest) request).getRequest().getRequestURI().toString();
+    }
+
+    private String getMethod(WebRequest request) {
+	return ((ServletWebRequest) request).getHttpMethod().toString();
     }
 }
--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/exception/ExceptionMessageRest.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/exception/ExceptionMessageRest.java
@@ -7,19 +7,23 @@ public class ExceptionMessageRest {
     private Date timestamp;
     private String message;
     private String additionalInformation;
+    private String path;
+    private String method;
 
     public ExceptionMessageRest() {
     }
 
-    public ExceptionMessageRest(Date timestamp, String message) {
+    public ExceptionMessageRest(Date timestamp, String path, String method, String message) {
 	this.timestamp = timestamp;
+	this.path = path;
+	this.setMethod(method);
 	this.message = message;
 
     }
 
-    public ExceptionMessageRest(Date timestamp, String message, String additionalInformation) {
-	this.timestamp = timestamp;
-	this.message = message;
+    public ExceptionMessageRest(Date timestamp, String path, String method, String message,
+	    String additionalInformation) {
+	this(timestamp, path, method, message);
 	this.additionalInformation = additionalInformation;
     }
 
@@ -45,6 +49,22 @@ public class ExceptionMessageRest {
 
     public void setAdditionalInformation(String additionalInformation) {
 	this.additionalInformation = additionalInformation;
+    }
+
+    public String getPath() {
+	return path;
+    }
+
+    public void setPath(String path) {
+	this.path = path;
+    }
+
+    public String getMethod() {
+	return method;
+    }
+
+    public void setMethod(String method) {
+	this.method = method;
     }
 
 }
