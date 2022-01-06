a785f6e22e1a320534f7420c3085437449363cc5
Georgi Velikov <gmvelikov@gmail.com>
2022-01-06 14:49:53 +0200

    Allowing CORS

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
@@ -1,5 +1,8 @@
 package com.appsdeveloperblog.app.ws.security;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.appsdeveloperblog.app.ws.service.UserService;
 
@@ -23,7 +29,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-	http.csrf()
+	http.cors()
+		.and()
+		.csrf()
 		.disable()
 		.authorizeRequests()
 		.antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
@@ -56,5 +64,20 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     // Returns AuthenticationFilter with Spring default login url '/login'
     public AuthenticationFilter getDefaultAuthenticationFilter() throws Exception {
 	return getAuthenticationFilter(null);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+	final CorsConfiguration configuration = new CorsConfiguration();
+
+	configuration.setAllowedOrigins(Arrays.asList("*"));
+	configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD"));
+	configuration.setAllowCredentials(true);
+	configuration.setAllowedHeaders(Arrays.asList("*"));
+
+	final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	source.registerCorsConfiguration("/**", configuration);
+
+	return source;
     }
 }
